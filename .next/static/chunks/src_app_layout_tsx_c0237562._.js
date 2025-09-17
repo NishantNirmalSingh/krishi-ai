f2654eebc14e74550e9472a31551cd1d695209cd
Nishant Nirmal_23_CSE-DS_067 <nishant4245@gmail.com>
2025-09-17 19:28:45 +0000

    make the website a little colourful and eye appleaing use such colour wh

--- .next/static/chunks/src_app_layout_tsx_c0237562._.js
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/src_app_globals_b805903d.css",
+  "static/chunks/[root-of-the-server]__71053b82._.css",
   "static/chunks/src_39a94ef1._.js",
   "static/chunks/node_modules_91319ed2._.js"
 ],
