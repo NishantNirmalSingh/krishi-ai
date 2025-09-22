c4f99915aff065e20c332d3886a25ff9ae5b9e0e
Nishant Nirmal_23_CSE-DS_067 <nishant4245@gmail.com>
2025-09-22 11:15:13 +0000

    i want you to incorporate bhojpuri language also everywhere so that user

--- src/lib/languages.ts
+++ src/lib/languages.ts
@@ -2,6 +2,7 @@
 export const languages = [
   { value: 'Assamese', label: 'অসমীয়া (Assamese)', code: 'as-IN' },
   { value: 'Bengali', label: 'বাংলা (Bengali)', code: 'bn-IN' },
+  { value: 'Bhojpuri', label: 'भोजपुरी (Bhojpuri)', code: 'bho-IN' },
   { value: 'English', label: 'English', code: 'en-US' },
   { value: 'Gujarati', label: 'ગુજરાતી (Gujarati)', code: 'gu-IN' },
   { value: 'Hindi', label: 'हिंदी (Hindi)', code: 'hi-IN' },
